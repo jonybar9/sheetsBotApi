68033e660927932eb3b944ced60c95597950c2a4
Jonatan Barr <jonybh99@gmail.com>
2019-03-11 14:15:16 +0200

    try2

--- server.js
+++ server.js
@@ -1,34 +1,57 @@
 const express = require('express')
-// const cors = require('cors')
-const bodyParser = require('body-parser')
-
 var app = express()
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-// app.use(cors({
-//     origin: '*',
-//     // credentials: true // enable set cookie
-// }));
+// var url = require('url');
+const request = require('request');
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/api', (req, res) => {
-    let payload = req.body;
-    console.log('checking for it')
-    console.log('check:', req.body)
-    if (payload.event.type === "app_mention") {
-        // if (payload.event.text.includes("tell me a joke")) {
-        // Make call to chat.postMessage using bot's token
-        console.log('yeahhh it was that')
-        // }
-    }
-    res.sendStatus(200);
-})
+app.set('port', (process.env.PORT || 9001));
 
-app.get('/hey', (req, res) => {
-    res.send('<div><h1>THE RAMONES RULE!</h1></div>')
-})
+app.get('/', function (req, res) {
+    res.send('It works!');
+});
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-    console.log('listening on', PORT)
-})
+app.post('/api', function (req, res) {
+    console.log('i am here')
+    console.log('got req', req)
+    // var parsed_url = url.format({
+    //     pathname: 'https://api.genius.com/search',
+    //     query: {
+    //         access_token: process.env.GENIUS_ACCESS,
+    //         q: req.body.text
+    //     }
+    // });
+
+    // request(parsed_url, function (error, response, body) {
+    //     if (!error && response.statusCode == 200) {
+    //         var data = JSON.parse(body);
+    //         var first_url = data.response.hits[0].result.url;
+
+    //         var body = {
+    //             response_type: "in_channel",
+    //             text: first_url
+    //         };
+
+    //         res.send(body);
+    //     }
+    // });
+});
+
+// app.post('/api', (req, res) => {
+//     let payload = req.body;
+//     console.log('checking for it')
+//     console.log('check:', req.body)
+//     res.redirect('https://slack.com/oauth/authorize');
+//     if (payload.event.type === "app_mention") {
+//         // if (payload.event.text.includes("tell me a joke")) {
+//         // Make call to chat.postMessage using bot's token
+//         console.log('yeahhh it was that')
+//         // }
+//     }
+// })
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
